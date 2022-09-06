2ea0781c6f755287c866da70bd5504698e462bb5
boring-km <kms0644804@naver.com>
2022-09-07 01:27:12 +0900

    확인 완료!

---
+++ README.md
@@ -0,0 +1,6 @@
+# Apple Login 서버 테스트용
+
+- domain이 필요하기 때문에 무료로 잠깐 열어주는 구름 IDE 서비스 사용함
+- 참고 글
+  - https://juhi.tistory.com/12
+  - https://github.com/ananay/apple-auth-example/blob/master/src/app.js
---
+++ main.js
@@ -0,0 +1,60 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+const jwt = require('jsonwebtoken');
+const bodyParser = require('body-parser');
+const fs = require('fs');
+const config = fs.readFileSync('./config.json');
+const AppleAuth = require('apple-auth');
+
+const auth = new AppleAuth(config, fs.readFileSync('키 파일 경로').toString(), 'text');
+
+
+app.get('/', (req, res) => {
+  res.redirect(auth.loginURL());
+});
+
+app.get('/token', (req, res) => {
+  res.send(auth._tokenGenerator.generate());
+});
+
+app.post('/callback', bodyParser(), async (req, res) => {
+
+  try {
+    console.log( Date().toString() + "GET /auth");
+    const response = await auth.accessToken(req.body.code);
+    const idToken = jwt.decode(response.id_token);
+
+    const user = {};
+    user.id = idToken.sub;
+
+    if (idToken.email) user.email = idToken.email;
+    if (req.body.user) {
+      const { name } = JSON.parse(req.body.user);
+      user.name = name;
+    }
+    // console.log();
+    res.json(user);
+  } catch (ex) {
+    console.error(ex);
+    res.send("An error occurred!");
+  }
+});
+
+app.get('/refresh', async (req, res) => {
+  try {
+    console.log( Date().toString() + "GET /refresh");
+    const accessToken = await auth.refreshToken(req.query.refreshToken);
+    res.json(accessToken);
+  } catch (ex) {
+    console.error(ex);
+    res.send("An error occurred!");
+  }
+});
+
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
